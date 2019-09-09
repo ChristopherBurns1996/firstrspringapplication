e08450c4beddb27660a1f6a0c046725880106e3f
Chris <Christopher.Burns@academytrainee.com>
2019-09-09 11:24:43 +0100

    Added controllers package added home controller added a / URL

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -3,6 +3,7 @@ package com.qa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//this apparently brings everything with this annotation into scope/context
 @SpringBootApplication
 public class App 
 {
